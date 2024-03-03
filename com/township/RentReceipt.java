1b5d585672adf65a641f16534f8d5fd5e9ccba51
Moses Rupak Raj Vedala <mosesvrr@gmail.com>
2024-03-03 15:22:02 -0500

    clean RentReceipt

--- com/township/RentReceipt.java
+++ com/township/RentReceipt.java
@@ -6,18 +6,10 @@ class RentReceipt extends Receipts {
     private Property property;
     private int rentDuration;
 
-    public RentReceipt(Property property){
-        super();
-        this.property=property;
-    }
-
     public RentReceipt(String id, double amount, User user, String notes, Date createdOn, Date updatedOn, Property property, int rentDuration) {
         super(id, amount, user, notes, createdOn, updatedOn);
         this.property = property;
         this.rentDuration = rentDuration;
-    }
-
-    public RentReceipt(String number, int i, User Moses, String monthlyRentPayment, Date date, Date date1) {
     }
 
     public Property getProperty() {
